6ca6e9906201bbea91760e00ebdcb011369f9d70
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-04-02 13:14:01 +0700

    Update KoplaxsSelf.py

--- KoplaxsSelf.py
+++ KoplaxsSelf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import LINETCR
+#import LINETCR
 from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re,os,subprocess
